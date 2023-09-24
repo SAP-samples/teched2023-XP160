412e1c4cb06b958db8782e2a38dfa28194853276
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-09-24 13:10:32 +0200

    feat: source code for tracking API

---
+++ azfunc/src/functions/fetchTrackingStatus.ts
@@ -0,0 +1,84 @@
+import { app } from "@azure/functions";
+const apiKey = process.env["TRACKING_API_KEY"];
+const trackingUrl = process.env["TRACKING_URL"];
+const useMock = process.env["USE_MOCK"];
+
+app.http("fetchTrackingStatus", {
+	methods: ["GET"],
+	authLevel: "function",
+	handler: async (request, context) => {
+		context.log(
+			`HTTP triggered function processed request for url "${request.url}"`,
+		);
+
+		const trackingId = request.query.get("trackingId");
+
+		if (!trackingId) {
+			return {
+				status: 400,
+				body: "Missing trackingId",
+			};
+		}
+
+		const responseBody = getTrackingStatus(trackingId);
+
+		context.log(`Response body: ${JSON.stringify(responseBody)}`);
+		return {
+			status: 200,
+			jsonBody: responseBody,
+		};
+	},
+});
+
+function getTrackingStatus(trackingId: string) {
+	if (useMock) {
+		const random = Math.floor(Math.random() * 4);
+		return getEntryById(random, trackingId);
+	}
+}
+
+function getEntryById(id: number, trackingId: string) {
+	const deliveryStatusMock = [
+		{
+			id: "",
+			service: "express",
+			statusCode: "transit",
+			status: "customs inspection",
+			remark: "The shipment is pending completion of customs inspection.",
+			nextSteps: "The status will be updated following customs inspection.",
+			estimatedTimeOfDelivery: "2023-11-06T00:00:00Z",
+		},
+		{
+			id: "",
+			service: "express",
+			statusCode: "delivered",
+			status: "delivered",
+			remark: "The shipment is successfully delivered.",
+			nextSteps: "",
+			estimatedTimeOfDelivery: "2023-11-02T00:00:00Z",
+		},
+		{
+			id: "",
+			service: "freight",
+			statusCode: "delivered",
+			status: "delivered",
+			remark: "The shipment is successfully delivered.",
+			nextSteps: "",
+			estimatedTimeOfDelivery: "2023-10-30T00:00:00Z",
+		},
+		{
+			id: "",
+			service: "express",
+			statusCode: "announced",
+			status: "sender announced shipment",
+			remark: "The shipment was announced by the sender at pick up station.",
+			nextSteps: "package to be picked up",
+			estimatedTimeOfDelivery: "2023-11-17T00:00:00Z",
+		},
+	];
+
+	const entry = deliveryStatusMock[id];
+	entry.id = trackingId;
+	//return JSON.stringify(entry);
+	return entry;
+}
