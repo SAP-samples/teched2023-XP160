9e9c27573eb9240b6db17b7a49d0fd1f6dcdf073
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-10-17 09:14:49 +0200

    docu: rework exercise 0

--- code/exercise1/infra_exercise1/main.tf
+++ code/exercise1/infra_exercise1/main.tf
@@ -25,56 +25,69 @@ resource "cloudfoundry_service_key" "privatelink" {
   service_instance = module.create_cf_service_instance_privatelink.id
 }
 
-
 # ------------------------------------------------------------------------------------------------------
 # 1.3.0 Create destination service + destination to S/4HANA Cloud system
 # ------------------------------------------------------------------------------------------------------
-# module "create_cf_service_instance_destination" {
-#   source       = "../../admin/modules/cloudfoundry/cf-service-instance"
-#   cf_space_id  = data.cloudfoundry_space.dev.id
-#   service_name = "destination"
-#   plan_name    = "lite"
-#   parameters = jsonencode({
-#     HTML5Runtime_enabled = true
-#     init_data = {
-#       subaccount = {
-#         existing_destinations_policy = "update"
-#         destinations = [
-#           {
-#             Name                     = "SAP-Build-Apps-Runtime"
-#             Type                     = "HTTP"
-#             Description              = "Endpoint to SAP S/4HANA Cloud System"
-#             URL                      = "https://sap.com"
-#             ProxyType                = "Internet"
-#             Authentication           = "NoAuthentication"
-#             "HTML5.ForwardAuthToken" = true
-#           }
-#         ]
-#       }
-#     }
-#   })
-# }
-
-
-
+#module "create_cf_service_instance_destination" {
+#  source       = "../../admin/modules/cloudfoundry/cf-service-instance"
+#  cf_space_id  = data.cloudfoundry_space.dev.id
+#  service_name = "destination"
+#  plan_name    = "lite"
+#  parameters = jsonencode({
+#    "HTML5Runtime_enabled" : "true",
+#    "init_data" : {
+#      "instance" : {
+#        "existing_destinations_policy" : "update",
+#        "destinations" : [
+#          {
+#            "Authentication"           = "BasicAuthentication",
+#            "Name"                     = "s4-on-azure",
+#            "Description"              = "SAP S/4HANA Connection via Private Link",
+#            "ProxyType"                = "PrivateLink",
+#            "Type"                     = "HTTP",
+#            "URL"                      = "http://93549d77-6851-4178-ba3c-18720c5e5638.p3.pls.sap.internal:50000",
+#            "User"                     = "BPINST"
+#            "Password"                 = "${var.s4_connection_pw}"
+#            "HTML5.DynamicDestination" = "true"
+#            "sap-client"               = "100"
+#          }
+#        ]
+#      }
+#    }
+#  })
+#}
 
 # ------------------------------------------------------------------------------------------------------
 # 1.4.1 Create service instance for SAP Build Workzone, standard edition
 # ------------------------------------------------------------------------------------------------------
 # Create app subscription to SAP Build Workzone, standard edition (depends on entitlement)
-# resource "btp_subaccount_subscription" "build_workzone" {
-#   subaccount_id = var.subaccount_id
-#   app_name      = "SAPLaunchpad"
-#   plan_name     = "standard"
-# }
+#resource "btp_subaccount_subscription" "build_workzone" {
+#  subaccount_id = var.subaccount_id
+#  app_name      = "SAPLaunchpad"
+#  plan_name     = "standard"
+#}
 
 # ------------------------------------------------------------------------------------------------------
 # 1.4.2 Give user Admin rights for SAP Build Workzone, standard edition
 # ------------------------------------------------------------------------------------------------------
 # Assign users to Role Collection: Launchpad_Admin
-# resource "btp_subaccount_role_collection_assignment" "launchpad_admin" {
-#   subaccount_id        = var.subaccount_id
-#   role_collection_name = "Launchpad_Admin"
-#   user_name            = var.username
-#   depends_on           = [btp_subaccount_subscription.build_workzone]
-# }
+#resource "btp_subaccount_role_collection_assignment" "launchpad_admin" {
+#  subaccount_id        = var.subaccount_id
+#  role_collection_name = "Launchpad_Admin"
+#  user_name            = var.username
+#  depends_on           = [btp_subaccount_subscription.build_workzone]
+#}
+
+# ------------------------------------------------------------------------------------------------------
+# 1.4.3 Deploy Cloud Foundry App via cf deploy
+# ------------------------------------------------------------------------------------------------------
+#resource "null_resource" "cf_app_deploy" {
+#  provisioner "local-exec" {
+#    command = "cf login -a https://api.cf.${var.region}.hana.ondemand.com -u ${var.username} -p ${var.cf_password}"
+#  }
+#
+#  provisioner "local-exec" {
+#    command = "cf deploy ../salesorder-navigator/mta.tar"
+#  }
+#
+#}
--- exercises/exercise0/README.md
+++ exercises/exercise0/README.md
@@ -1,48 +1,47 @@
 # Exercise 0 - Prepare your development environment
 
-In this exercise, we will connect an UI5 app running on SAP BTP to an SAP S/4HANA Cloud system running on Microsoft Azure.
+In this workshop, we will connect an UI5 app running on SAP BTP to an SAP S/4HANA Cloud system running on Microsoft Azure. 
 
 ## Exercise 0.1: Get the code for this session into VS Code
 
+We will use GitHub [Codespaces](https://docs.github.com/codespaces/overview) as integrated development environment for this workshop. Access the Codespace following these steps:
+
 1. Open the [GitHub repository for XP160](https://github.com/SAP-samples/teched2023-XP160).
 
-![](/exercises/exercise0/images/00_01_01.png)
+    ![Screenshot of GitHub repository XP160](/exercises/exercise0/images/00_01_01.png)
 
 2. Click on the `Code` button, switch to the `Codespaces` tab and click on the button `Create codespace for ...`.
 
-    This will take a few minutes. Be patient :-)
+    This will take a few minutes. Be patient 🙂
 
-![](/exercises/exercise0/images/00_01_02.png)
+    ![Screenshot of navigation to Codespace creation in the repository XP160](/exercises/exercise0/images/00_01_02.png)
 
 3. While the codespace is created for you, you will see this screen
 
-![](/exercises/exercise0/images/00_01_03.png)
+    ![Screenshot of setup screen for codespace](/exercises/exercise0/images/00_01_03.png)
 
 4. Once all is done, you are in your Codespace.
 
     > **Note** - GitHub codespaces are free for a certain amount of time per month. For the hands-on session the free time is more than enough. **Don't forget to delete your codespace again after the hands-on session!**
 
-![](/exercises/exercise0/images/00_01_04.png)
+    ![Screenshot of GitHub Codespace view on the respository XP160](/exercises/exercise0/images/00_01_04.png)
 
 
-## Exercise 0.2: get the ID of your subaccount
+## Exercise 0.2: Get the ID of your SAP BTP subaccount
 
-1. Get the credentials from the instructor of this hands-on session
+1. Get the credentials (username and password) from the instructor of this hands-on session. Each participant will get access to one SAP BTP subaccount.
 
-    You will get the username and your password from the session instructors. Each participant will get access to one BTP subaccount.
+2. Open the [SAP BTP global account for XP160](https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/a0ab1ce3-9dab-48b8-9122-524f7fde1f28/) in a **new tab** of your web browser. You will have to use the credentials provided to you by the instructors.
 
-2. Open the [SAP BTP global account for XP160](https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/a0ab1ce3-9dab-48b8-9122-524f7fde1f28/) in a **new tab** of your web browser.
-    You will have to use the credentials provided to you by the instructors.
-![List of Subaccounts](/exercises/exercise0/images/00_02_01.png)
+    ![Screenshot of list of subaccounts](/exercises/exercise0/images/00_02_01.png)
 
 3. Select your sub account and copy the `Subaccount ID` into the clipboard.
 
-![](/exercises/exercise0/images/00_02_02.png)
+   ![Screenshot of SAP BTP Subaccount overview](/exercises/exercise0/images/00_02_02.png)
 
 
 ## Summary
 
 You've now prepared your development environment and have all information to finally start using Terraform.  
 
-Continue to - [Exercise 1 - Exercise 2 Description](../ex2/README.md)
-
+Continue to - [Exercise 1 - Exercise 2 Description](../exercise1/README.md)
